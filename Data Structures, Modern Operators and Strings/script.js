ea0e36bae8ad78d5ae5d532e6c1aa3bc03109649
Mohit <msaini0r@gmail.com>
2022-01-05 19:41:37 +0530

    changes

--- Data Structures, Modern Operators and Strings/script.js
+++ Data Structures, Modern Operators and Strings/script.js
@@ -1,13 +1,33 @@
 'use strict';
 
 const restaurant = {
-    name: 'Classico Italiano',
+    names: 'Classico Italiano',
     location: 'Via Angelo Tavanti 23, Firenze, Italy',
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto']
-};
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+          open: 12,
+          close: 22,
+        },
+        fri: {
+          open: 11,
+          close: 23,
+        },
+        sat: {
+          open: 0, // Open 24 hours
+          close: 24,
+        },
+      },
+    
 
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
+
+};
+/* ----------------------------Destructing Arrays (lecture-3) ------------------------- */
 // destructing basically means to breeak a complex data structure ddown into a small data structure like a variable.
 
 const arr = [2, 3, 4];
@@ -15,7 +35,59 @@ const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-const [x,y,z] = arr;
+const [x, y, z] = arr;
 console.log(x, y, z);
 
+
+// IN order to skip the second element we use only commas and the second element willl become third.
+let [first, , second] = restaurant.categories;
+console.log(first, second);
+
+// Inorder to switch two elements we use this code below and there is other way also by creating a temp variable but here we are not using it.
+
+[first, second] = [second, first]
+console.log(first, second);
+
+// returns 2 values from a function
+const [starter, maincourse] = restaurant.order(2, 0);
+console.log(starter, maincourse);
+
+// nested basically means array in array.
+const nested = [2, 4, [7, 8]];
+// const [i, ,j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+/*---------------------------------Destructuring Objects (lec-4)------------------------------*/
+
+// here order does not matter
+const { names, openingHours, categories} = restaurant;
+console.log(names, openingHours, categories);
+
+// to change the names
+
+const{
+    names: restaurantName,
+    openingHours: Hours,
+    categories: tags,
+} = restaurant;
+console.log(restaurantName,Hours,tags);
+
+// we can create custom elemnts also
+
+const { drinks = ['coke','pepsi']} = restaurant;
+console.log(drinks);
+
+// Mutating variables
+let d = 111;
+let f = 99;
+const obj = {d:23, f:7, c:45};
+({d,f} = obj);
+console.log(a,b);
+
+// Nested Objects
+
+const{
+    fri: {open, close}, } = openingHours;
+console.log(open, close);
 
