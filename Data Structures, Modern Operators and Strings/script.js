099f84300181c2d1180a1c3ddd9bee933f26ba9a
Mohit <msaini0r@gmail.com>
2022-01-05 18:01:35 +0530

    changes

--- Data Structures, Modern Operators and Strings/script.js
+++ Data Structures, Modern Operators and Strings/script.js
@@ -7,3 +7,15 @@ const restaurant = {
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto']
 };
+
+// destructing basically means to breeak a complex data structure ddown into a small data structure like a variable.
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x,y,z] = arr;
+console.log(x, y, z);
+
+
